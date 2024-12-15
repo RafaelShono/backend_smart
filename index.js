4fd73487e4ae3f87ec962ecc5303ce376dc8c830
rafaelShono <rafael.ifpr.2019@gmail.com>
2024-12-15 12:15:51 -0300

    mudana na forma de inicar server

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-// index.js
-
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
 
-// O middleware para o webhook precisa do corpo bruto da requisição
+// Configurar CORS permitindo o domínio do seu front-end hospedado no Vercel
+app.use(cors({
+  origin: 'https://redacaosmart-nz4uzaz7m-rafaelshonos-projects.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
+// Middleware especial para a rota /webhook, que precisa do corpo bruto da requisição
 app.use((req, res, next) => {
   if (req.originalUrl === '/webhook') {
     next();
@@ -18,19 +22,21 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   res.removeHeader('Cross-Origin-Opener-Policy');
-  // ou para definir como 'unsafe-none'
   res.setHeader('Cross-Origin-Opener-Policy', 'unsafe-none');
   next();
 });
+
 // Rotas
 const analyzeRoutes = require('./routes/analyze');
 const paymentRoutes = require('./routes/payments');
-const enviarEmailRouter = require('./routes/enviarEmail'); // Novo
+const enviarEmailRouter = require('./routes/enviarEmail');
 
 // Usar as rotas
 app.use('/', analyzeRoutes);
 app.use('/', paymentRoutes);
-app.use('/api', enviarEmailRouter); // Novo
+app.use('/api', enviarEmailRouter);
+
+// Iniciar o servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
